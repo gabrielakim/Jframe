fee389d78b5d24ce154ec0759367a49ddd8a5563
gabrielakim <60987477+gabrielakim@users.noreply.github.com>
2020-03-04 19:17:39 -0300

    Add files via upload

---
+++ LabelTest.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+public class LabelTest
+{
+  public static void main( String[] args )  
+  {
+      LabelFrame labelFrame = new LabelFrame();
+      LabelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      LabelFrame.setSize( 260, 180 );
+      LabelFrame.setViseble( true );
+  }
+
+    
+}
